052a4031c4073ec38fd07f28433348601004a337
rodrigomakoin <48112596+rodrigomakoin@users.noreply.github.com>
2019-11-13 15:11:24 -0300

    Add files via upload

---
+++ atv10.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+main(){
+	int vet[5];
+	int i,num;
+	
+	for(i=0;i<5;i++)
+	{
+		printf("\n Digite um numero: ", i);
+		scanf("%d", &vet[i]);
+	}
+	if( num = 0)
+	{
+		printf("zero: %d",num);
+	}
+	
+	return (0);
+	
+}
